43c6237fa40458e279a3b0314a04a932ec4a62cf
Orhan Yalcin <orhan.yalcin@student.vu.nl>
2018-07-14 10:23:31 +0300

    Update titanic_under_20.py

--- titanic_under_20/titanic_under_20.py
+++ titanic_under_20/titanic_under_20.py
@@ -4,18 +4,14 @@ train = train.dropna()
 y = train['Survived']
 X = train.drop(['Survived', 'PassengerId', 'Name', 'Ticket', 'Cabin'], 1, inplace=True)
 X = pd.get_dummies(train)
-
 from sklearn import tree
 dtc = tree.DecisionTreeClassifier()
 dtc.fit(X, y)
-
 test = pd.read_csv("test.csv")
 ids = test[['PassengerId']]
 test.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], 1, inplace=True)
 test.fillna(2, inplace=True)
 test = pd.get_dummies(test)
-
 predictions = dtc.predict(test)
-
 results = ids.assign(Survived = predictions)
 results.to_csv("titanic-results.csv", index=False)
