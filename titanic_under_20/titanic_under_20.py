ede7bb266f6bb2980303eb226cb7b1dbf351fc5a
Orhan Yalcin <orhan.yalcin@student.vu.nl>
2018-07-14 09:33:58 +0300

    Add files via upload

---
+++ titanic_under_20/titanic_under_20.py
@@ -0,0 +1,19 @@
+import pandas as pd
+train = pd.read_csv("train.csv")
+train = train.dropna()
+y = train['Survived']
+X = train.drop(['Survived', 'PassengerId', 'Name', 'Ticket', 'Cabin'], 1, inplace=True)
+X = pd.get_dummies(train)
+from sklearn import tree
+dtc = tree.DecisionTreeClassifier()
+dtc.fit(X, y)
+
+test = pd.read_csv("test.csv")
+ids = test[['PassengerId']]
+test.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], 1, inplace=True)
+test.fillna(2, inplace=True)
+test = pd.get_dummies(test)
+
+predictions = dtc.predict(test)
+results = ids.assign(Survived = predictions)
+results.to_csv("titanic-results.csv", index=False)
